fd58bf67671e525c609ac32ba2a83dbe20f143f5
Mugishaa77 <110553687+Mugishaa77@users.noreply.github.com>
2023-01-12 16:24:31 +0300

    Included skeleton of the website landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import React from "react";
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <div className="Container">
+      <h1>Welcome to Kitchen 001</h1>
+      <div className="menu">
+        <button type="button" class="btn btn-warning">Menu</button>
+      </div>
+      <div className="online-menu">
+        <button type="button" class="btn btn-success">Online Menu</button>
+      </div>
+      <div className="reservations">
+        <button type="button" class="btn btn-danger">Reservations</button>
+      </div>
+      <div className="pay">
+        <button class="btn btn-dark">Click here to Pay</button>
+      </div>
+      <div className="socials">
+        <span>Instagram</span>
+        <span>Twitter</span>
+        <span>Phone</span>
+        <span>Email</span>
+      </div>
+      </div>
     </div>
   );
 }
